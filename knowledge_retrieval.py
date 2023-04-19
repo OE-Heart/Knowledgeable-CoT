5e08d7eeb880b6440adf3a45846b5cf39cd7d6b8
OE-Heart <leoouyixin@126.com>
2023-04-19 16:51:21 +0800

    update knowledge_retrieval

--- knowledge_retrieval.py
+++ knowledge_retrieval.py
@@ -1,13 +1,16 @@
 import os
+import re
 import json
+import spacy
+import argparse
 from tqdm import tqdm
 from pyserini.search.lucene import LuceneSearcher
 
 from utils import *
 
 
-def retrieval(query):
-    hits = ssearcher.search(query, 5)
+def retrieval(query, k=5):
+    hits = ssearcher.search(query, k)
     paragraphs = []
     for i in range(len(hits)):
         doc = ssearcher.doc(hits[i].docid)
@@ -17,54 +20,82 @@ def retrieval(query):
 
 
 if __name__ == "__main__":
-    input_path = "data/scienceqa"
-    output_path = "data"
-    output_name = "knowledge.json"
 
-    problems = json.load(open(os.path.join(input_path, "problems.json")))
-    pid_splits = json.load(open(os.path.join(input_path, "pid_splits.json")))
-    visual_clues = json.load(open(os.path.join(output_path, "visual_clues.json")))["visual_clues"]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_file", type=str, default="data/scienceqa/problems.json")
+    parser.add_argument(
+        "--result_file",
+        type=str,
+        default="results/chatgpt/test/QCM-ALE/visual_clues.json/paths_10/2shots_seed_3.json",
+    )
+    parser.add_argument("--knowledge_store", type=str, default="wikipedia-dpr")
+    parser.add_argument(
+        "--knowledge_store_path",
+        type=str,
+        default="indexes/index-wikipedia-dpr-20210120-d1b9e6",
+    )
+    parser.add_argument("--k_hits", type=int, default=5)
+    parser.add_argument("--save_every", type=int, default=10)
+    args = parser.parse_args()
 
-    knowledge_store = "wikipedia-dpr"
-    # ssearcher = LuceneSearcher.from_prebuilt_index(knowledge_store)
+    results = json.load(open(args.result_file))["results"]
+    outputs = json.load(open(args.result_file))["outputs"]
 
-    knowledge_store_path = "indexes/index-wikipedia-dpr-20210120-d1b9e6"
-    ssearcher = LuceneSearcher(knowledge_store_path)
+    # ssearcher = LuceneSearcher.from_prebuilt_index(args.knowledge_store)
+    ssearcher = LuceneSearcher(args.knowledge_store_path)
+
+    nlp = spacy.load("en_core_web_md")
+
+    output_path = os.path.dirname(args.result_file)
+    output_name = os.path.basename(args.result_file).replace(
+        ".json", "_external_knowledge.json"
+    )
+    output_file = os.path.join(output_path, output_name)
+
+    # load the check point
+    if os.path.exists(output_file):
+        print("# The result file exists! We will load the check point!!!")
+        check_point = json.load(open(output_file))
+
+        external_knowledge = check_point["external_knowledge"]
+        if len(external_knowledge) >= len(results):
+            print("## The result file is complete! We will exit!!!")
+            exit()
+        else:
+            print("## The result file is not complete! We will continue!!!")
+    else:
+        external_knowledge = {}
 
     ## Retrieve external knowledge
-    external_knowledge = {}
+    print("Retrieving external knowledge!")
+    for i, pid in enumerate(tqdm(outputs)):
+        if pid in external_knowledge:
+            print(f"## The external knowledge of {pid} is already retrieved!")
+            continue
 
-    print(f"Retrieving external knowledge!")
-    for pid in tqdm(problems):
-        problems[pid]["visual_clues"] = visual_clues[pid] if pid in visual_clues else ""
+        external_knowledge[pid] = []
+        for reasoning_path in outputs[pid]:
+            explanation = re.sub(r"The answer is [A-Z]. BECAUSE: ", "", reasoning_path)
+            nlp_text = nlp(explanation).sents
+            sent_paragraphs = []
+            for sent in nlp_text:
+                sent = str(sent)
+                try:
+                    paragraphs = retrieval(sent, args.k_hits)
+                    sent_paragraphs.append((sent, paragraphs))
+                except Exception as e:
+                    print(sent)
+                    print(e)
+            external_knowledge[pid].append(sent_paragraphs)
 
-        question = get_question_text(problems[pid])
-        hint = get_hint_text(problems[pid])
-        visual_clues = get_visual_clues(problems[pid])
+        ## Save the external knowledge
+        if (i + 1) % args.save_every == 0 or (i + 1) == len(outputs):
+            print(f"Saved to {output_file}")
 
-        sent_paragraphs = []
-        for sent in [question, hint, visual_clues]:
-            sent = str(sent)
-            if sent == "":
-                continue
-            try:
-                paragraphs = retrieval(sent)
-                sent_paragraphs.append((sent, paragraphs))
-            except Exception as e:
-                print(sent)
-                print(e)
-        external_knowledge[pid] = sent_paragraphs
-        # print(external_knowledge[pid])
+            knowledge = {
+                "knowledge_store": args.knowledge_store,
+                "external_knowledge": external_knowledge,
+            }
 
-    ## Save the external knowledge
-    output_file = os.path.join(output_path, output_name)
-    os.makedirs(output_path, exist_ok=True)
-    print(f"Saved to {output_file}")
-
-    results = {
-        "knowledge_store": knowledge_store,
-        "external_knowledge": external_knowledge,
-    }
-
-    with open(output_file, "w") as f:
-        json.dump(results, f, indent=2, separators=(",", ": "))
+            with open(output_file, "w") as f:
+                json.dump(knowledge, f, indent=2, separators=(",", ": "))
